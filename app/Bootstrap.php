d6c96d0dce0566601107d37aa4783d3fdd24681c
andrstom <andrsak85@gmail.com>
2023-09-04 14:48:45 +0200

    - povolit debugging

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -10,7 +10,7 @@ class Bootstrap
     public static function boot(): Configurator
     {
         $configurator = new Configurator;
-        //$configurator->setDebugMode('91.237.237.12'); // enable for your remote IP
+        $configurator->setDebugMode('91.237.237.12'); // enable for your remote IP
         $configurator->enableTracy(__DIR__ . '/../log');
         $configurator->setTimeZone('Europe/Prague');
         $configurator->setTempDirectory(__DIR__ . '/../temp');
